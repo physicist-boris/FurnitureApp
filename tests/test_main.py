60368409746a78865dc57ec6f99aa67a738de46f
physicist-boris <elvisametolo@gmail.com>
2023-03-01 20:21:08 -0800

    testing correction: removed score prediction

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,8 +10,5 @@ client = TestClient(app)
 def test_create_predictions():
     response = client.post("/predict/furnitureimage", files = {"file":open(os.path.join(tests_directory_path, 'test_data/image_test.png'), "rb")})
     assert response.status_code == 200
-    assert response.json() == {
-    "model-prediction": "Sofa",
-    "model-prediction-confidence-score": 64.67121243476868
-}
+    assert response.json()['model-prediction'] == 'Sofa'
     
